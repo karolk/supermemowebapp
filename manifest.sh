318bbba9349de933c4b5ecf514ef4a3c9d7edffc
Karol Kowalski <kowcik@gmail.com>
2012-02-25 15:02:18 -0300

    md5 instead of date as a change trigger for offline manifest

--- manifest.sh
+++ manifest.sh
@@ -1,6 +1,6 @@
 echo 'CACHE MANIFEST' > offline.manifest
 h=#
-d=`date`
-echo $h$d >> offline.manifest
+stamp=`md5 * | md5`
+echo $h$stamp >> offline.manifest
 echo 'CACHE:' >> offline.manifest
 ls *.html *.css *.js | cat >> offline.manifest
